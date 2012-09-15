c4ed4dcd1a69b49c1d3192bf27fd66ccaafc76a6
Christopher Baker <me@christopherbaker.net>
2012-09-14 22:46:42 -0700

    Made the window a little smaller.

--- example-stock/src/main.cpp
+++ example-stock/src/main.cpp
@@ -6,7 +6,7 @@
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 800,700, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 320,240, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
