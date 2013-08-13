7255dddc84b97fdff4dcefb1e6dcd15a0de03e9e
Jeff Crouse <jefftimesten@gmail.com>
2013-08-13 06:56:50 -0400

    oops -- pushed some conflicts :(

--- src/ofxJSONElement.cpp
+++ src/ofxJSONElement.cpp
@@ -1,17 +1,6 @@
 #include "ofxJSONElement.h"
 
 
-<<<<<<< HEAD
-//--------------------------------------------------------------
-ofxJSONElement::ofxJSONElement(Json::Value& v) : Value(v)
-{
-
-}
-
-
-//--------------------------------------------------------------
-ofxJSONElement::ofxJSONElement(string jsonString)
-=======
 //------------------------------------------------------------------------------
 ofxJSONElement::ofxJSONElement(Json::Value& v) :
     Value(v)
@@ -20,20 +9,10 @@ ofxJSONElement::ofxJSONElement(Json::Value& v) :
 
 //------------------------------------------------------------------------------
 ofxJSONElement::ofxJSONElement(const std::string& jsonString)
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 {
 	parse(jsonString);
 }
 
-<<<<<<< HEAD
-
-//--------------------------------------------------------------
-bool ofxJSONElement::parse(string jsonString)
-{
-	Reader reader;
-	if(!reader.parse( jsonString, *this )) {
-		ofLog(OF_LOG_WARNING, "Unable to parse string");
-=======
 //------------------------------------------------------------------------------
 bool ofxJSONElement::parse(const std::string& jsonString)
 {
@@ -41,57 +20,22 @@ bool ofxJSONElement::parse(const std::string& jsonString)
 	
     if(!reader.parse( jsonString, *this )) {
 		ofLogError("ofxJSONElement::parse") << "Unable to parse string: " << reader.getFormattedErrorMessages();
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 		return false;
 	}
 	
     return true;
 }
 
-<<<<<<< HEAD
-
-//--------------------------------------------------------------
-bool ofxJSONElement::open(string filename)
-{
-	if(filename.find("http://")==0 )
-    {
-		return openRemote(filename);
-	}
-    else if(filename.find("https://")==0)
-    {
-		return openRemote(filename, true);
-	}
-    else
-    {
-=======
 //------------------------------------------------------------------------------
 bool ofxJSONElement::open(const string& filename)
 {
 	if(filename.find("http://") == 0 || filename.find("https://") == 0) {
 		return openRemote(filename);
 	} else {
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 		return openLocal(filename);
 	}
 }
 
-<<<<<<< HEAD
-
-//--------------------------------------------------------------
-bool ofxJSONElement::openLocal(string filename)
-{
-    ofBuffer buffer = ofBufferFromFile(filename);
-	if(buffer.size())
-    {
-        Reader reader;
-        if(!reader.parse( buffer.getText(), *this ))
-        {
-            ofLog(OF_LOG_WARNING, "Unable to parse "+filename);
-            return false;
-        }
-	} else {
-		ofLog(OF_LOG_ERROR, "Could not load file " + filename);
-=======
 //------------------------------------------------------------------------------
 bool ofxJSONElement::openLocal(const std::string& filename)
 {    
@@ -101,7 +45,6 @@ bool ofxJSONElement::openLocal(const std::string& filename)
     
 	if(!reader.parse( buffer.getText(), *this )) {
 		ofLogError("ofxJSONElement::openLocal") << "Unable to parse " << filename << ": " << reader.getFormattedErrorMessages();
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 		return false;
 	}
     
@@ -149,15 +92,8 @@ bool ofxJSONElement::save(const std::string& filename, bool pretty) const
     return true;
 }
 
-<<<<<<< HEAD
-
-
-//--------------------------------------------------------------
-string ofxJSONElement::getRawString(bool pretty)
-=======
 //------------------------------------------------------------------------------
 std::string ofxJSONElement::getRawString(bool pretty) const
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 {
 	std::string raw;
     
@@ -170,23 +106,4 @@ std::string ofxJSONElement::getRawString(bool pretty) const
 	}
     
 	return raw;
-}
-
-//--------------------------------------------------------------
-string ofxJSONElement::decodeURL(string &SRC)
-{
-    string ret;
-    char ch;
-    int i, ii;
-    for (i=0; i<SRC.length(); i++) {
-        if (int(SRC[i])==37) {
-            sscanf(SRC.substr(i+1,2).c_str(), "%x", &ii);
-            ch=static_cast<char>(ii);
-            ret+=ch;
-            i=i+2;
-        } else {
-            ret+=SRC[i];
-        }
-    }
-    return (ret);
 }
--- src/ofxJSONElement.h
+++ src/ofxJSONElement.h
@@ -10,10 +10,6 @@
  *
  */
 
-<<<<<<< HEAD
-#pragma once
-=======
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 
 #pragma once
 
@@ -23,11 +19,6 @@
 #include "ofLog.h"
 #include "ofURLFileLoader.h"
 
-<<<<<<< HEAD
-using namespace Json;
-extern "C" size_t decode_html_entities_utf8(char *dest, const char *src);
-=======
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 
 class ofxJSONElement : public Json::Value {
 public:
@@ -35,33 +26,11 @@ public:
 	ofxJSONElement(const std::string& jsonString);
 	ofxJSONElement(Json::Value& v);
 	
-<<<<<<< HEAD
-	bool parse(string jsonString);
-	bool open(string filename);
-	bool openLocal(string filename);
-	bool openRemote(string filename, bool secure=false);
-	bool save(string filename, bool pretty=false);
-	string getRawString(bool pretty=true);
-    
-=======
 	bool parse(const std::string& jsonString);
 	bool open(const std::string& filename);
 	bool openLocal(const std::string& filename);
 	bool openRemote(const std::string& filename);
 	bool save(const std::string& filename, bool pretty = false) const;
 	std::string getRawString(bool pretty = true) const;
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
 
-// static
-    static string decodeURL(string& str);
-    static string decodeEntities(string& str) {
-        char dest[ str.length() ];
-        decode_html_entities_utf8(dest, str.c_str());
-        return string( dest );
-    }
 };
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 51781d6609531071661c0f3bf167166ebcc34a4e
